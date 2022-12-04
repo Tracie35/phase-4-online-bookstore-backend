b83bbb6b2d0f7aa087a87dbd96beacbb58983004
calvin <ckodalo@gmail.com>
2022-12-04 10:20:17 +0300

    added reviews and users controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,5 +5,39 @@ class ReviewsController < ApplicationController
      render json: reviews
     end
 
-    
+
+    def show
+        rview = review_finder
+        # if power
+        render json: review
+        # else
+        # render json: { error: "Power not found" }, status: :not_found
+        # end
+    end
+
+    def update
+        review = review_finder
+        # if power
+            review.update!(book_params)
+            render json: review
+        # else
+        #     render json: { error: "Power not found" }, status: :not_found
+        # end
+    end
+
+    private
+
+        def review_finder
+            review = Review.find_by(id: params[:id])
+        end
+
+        def book_params
+            params.permit(:, :author, :genre, :image, :price, :description)
+          end
+
+        def render_not_found_response
+            render json: { error: "Review not found" }, status: :not_found
+        end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+
+    def index
+        users = Users.all
+        render json: users
+    end
+
+    def show
+        user = user_finder
+        # if power
+        render json: user
+        # else
+        # render json: { error: "Power not found" }, status: :not_found
+        # end
+    end
+
+    def update
+        user = user_finder
+        # if power
+            user.update!(user_params)
+            render json: user
+        # else
+        #     render json: { error: "Power not found" }, status: :not_found
+        # end
+    end
+
+    private
+
+        def user_finder
+            user = User.find_by(id: params[:id])
+        end
+
+        def user_params
+            params.permit(:name, :age)
+          end
+
+        def render_not_found_response
+            render json: { error: "User not found" }, status: :not_found
+        end
 end
