125153817351324e6ae20371713f236e1621de5f
Edward <edward.ochieng@student.moringaschool.com>
2022-12-10 12:41:01 +0300

    solved bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :reviews, only:[:update,:create,:destroy,:index]
   resources :users
-  resources :books, only:[:index]
+  resources :books, only:[:index, :show]
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
-  delete "/logout/:id", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
   delete "/reviews/:id", to: "reviews#destroy"
 
 
